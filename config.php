07121895b1782022592d95e6c013ec1a656b6df4
Raushan kumar Gupta <114905667+raushangrkg123@users.noreply.github.com>
2026-04-25 13:57:42 +0530

    Switch database connection from MySQL to PostgreSQL

--- config.php
+++ config.php
@@ -1,14 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "root";  // Default XAMPP user
-$password = "";  // Default empty
-$dbname = "portfolio_database";  // Database Name
+$host = "localhost";
+$port = "5432";
+$dbname = "portfolio_db";   // tumhara DB name (pgAdmin wala)
+$user = "postgres";         // default PostgreSQL user
+$password = "yourpassword"; // apna pgAdmin password
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die("Connection failed: " . pg_last_error());
 }
 ?>
